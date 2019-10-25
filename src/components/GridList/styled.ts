157d36ffbdfaf239e69b666a205655f544e2b87b
Lukasz Walewski <lukw34@gmail.com>
2019-10-25 17:02:56 +0200

    Add grid-card layout

--- src/components/GridList/styled.ts
+++ src/components/GridList/styled.ts
@@ -12,8 +12,8 @@ export const PokemonGridListItem = styled.div`
     height: 250px;
     margin: 10px;
     flex: 0 0 20%;
-    border: blue 2px solid;
-    display: flex:
+    box-shadow: -5px 5px 8px 1px rgba(0,0,0,0.4);
+    display: flex;
     flex-direction: row;
 `;
 
